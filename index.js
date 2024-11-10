5c6c1e33beb0690c0d006615d12a29adcf149375
shiabulislam <md.shiabul.cse@gmail.com>
2024-11-10 23:51:23 +0600

    es6 javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+var mySet=new Set();
+mySet.add("afganisthan");
+mySet.add("bangladesh");
+mySet.add("bangladesh");
+mySet.add("china");
+mySet.add("japan");
+mySet.add("japan");
+
+mySet.size;
+console.log(mySet.has("afganisthan"));
+
+
